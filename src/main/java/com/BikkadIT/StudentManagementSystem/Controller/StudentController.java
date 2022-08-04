792396ecf7ef6f336db3f256db21b8cc2ea73337
Somnath1225 <somnath.dinde98@gmail.com>
2022-08-04 10:54:35 +0530

    EntityClass

--- src/main/java/com/BikkadIT/StudentManagementSystem/Controller/StudentController.java
+++ src/main/java/com/BikkadIT/StudentManagementSystem/Controller/StudentController.java
@@ -3,8 +3,6 @@ package com.BikkadIT.StudentManagementSystem.Controller;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
-@Table(name="student_Details")
 public class StudentController {
 	
 	
