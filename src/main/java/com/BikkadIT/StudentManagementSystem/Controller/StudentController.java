d535e446ba98bf536b2594fafd37135274bb78b5
Somnath1225 <somnath.dinde98@gmail.com>
2022-08-04 10:44:10 +0530

    ok

--- src/main/java/com/BikkadIT/StudentManagementSystem/Controller/StudentController.java
+++ src/main/java/com/BikkadIT/StudentManagementSystem/Controller/StudentController.java
@@ -1,5 +1,12 @@
 package com.BikkadIT.StudentManagementSystem.Controller;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student_Details")
 public class StudentController {
+	
+	
 
 }
